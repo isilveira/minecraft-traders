d12f104110003aa1c676ae6832ce35956473b75f
Ítalo Silveira <italobrian@gmail.com>
2020-07-01 18:27:08 -0300

    Patch trade flow

---
+++ src/BAYSOFT.Core.Application/MinecraftTraders/Trades/Commands/PatchTrade/PatchTradeCommand.cs
@@ -0,0 +1,23 @@
+﻿using BAYSOFT.Core.Domain.Entities.MinecraftTraders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BAYSOFT.Core.Application.MinecraftTraders.Trades.Commands.PatchTrade
+{
+    public class PatchTradeCommand : ApplicationRequest<Trade, PatchTradeCommandResponse>
+    {
+        public PatchTradeCommand()
+        {
+            ConfigKeys(x => x.TradeID);
+
+            ConfigSuppressedProperties(x => x.Villager);
+            ConfigSuppressedProperties(x => x.Accepts);
+            ConfigSuppressedProperties(x => x.Offers);
+
+            ConfigSuppressedResponseProperties(x => x.Villager);
+            ConfigSuppressedResponseProperties(x => x.Accepts);
+            ConfigSuppressedResponseProperties(x => x.Offers);
+        }
+    }
+}
---
+++ src/BAYSOFT.Core.Application/MinecraftTraders/Trades/Commands/PatchTrade/PatchTradeCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using BAYSOFT.Core.Domain.Interfaces.Infrastructures.Contexts;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ModelWrapper.Extensions.Patch;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BAYSOFT.Core.Application.MinecraftTraders.Trades.Commands.PatchTrade
+{
+    public class PatchTradeCommandHandler : IRequestHandler<PatchTradeCommand, PatchTradeCommandResponse>
+    {
+        private IDefaultDbContext DefaultDbContext { get; set; }
+
+        public PatchTradeCommandHandler(IDefaultDbContext defaultDbContext)
+        {
+            DefaultDbContext = defaultDbContext;
+        }
+
+        public async Task<PatchTradeCommandResponse> Handle(PatchTradeCommand request, CancellationToken cancellationToken)
+        {
+            var id = request.Project(x => x.TradeID);
+
+            var data = await DefaultDbContext.Trades
+                .SingleOrDefaultAsync(x => x.TradeID == id, cancellationToken);
+
+            if (data==null)
+            {
+                throw new Exception("Trade not found!");
+            }
+
+            request.Patch(data);
+
+            await DefaultDbContext.SaveChangesAsync(cancellationToken);
+
+            return new PatchTradeCommandResponse(request, data, resultCount: 1);
+        }
+    }
+}
