76229b334fada7c0a9ed149d0e7b1be008cbbfd7
Ítalo Silveira <italobrian@gmail.com>
2020-07-01 23:11:50 -0300

    Patch accept flow

---
+++ src/BAYSOFT.Core.Application/MinecraftTraders/Accepts/Commands/PatchAccept/PatchAcceptCommand.cs
@@ -0,0 +1,14 @@
+﻿using BAYSOFT.Core.Domain.Entities.MinecraftTraders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BAYSOFT.Core.Application.MinecraftTraders.Accepts.Commands.PatchAccept
+{
+    public class PatchAcceptCommand : ApplicationRequest<Villager, PatchAcceptCommandResponse>
+    {
+        public PatchAcceptCommand()
+        {
+        }
+    }
+}
---
+++ src/BAYSOFT.Core.Application/MinecraftTraders/Accepts/Commands/PatchAccept/PatchAcceptCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using BAYSOFT.Core.Domain.Interfaces.Infrastructures.Contexts;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ModelWrapper.Extensions.Patch;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BAYSOFT.Core.Application.MinecraftTraders.Accepts.Commands.PatchAccept
+{
+    public class PatchAcceptCommandHandler : IRequestHandler<PatchAcceptCommand, PatchAcceptCommandResponse>
+    {
+        private IDefaultDbContext DefaultDbContext { get; set; }
+        public PatchAcceptCommandHandler(IDefaultDbContext defaultDbContext)
+        {
+            DefaultDbContext = defaultDbContext;
+        }
+
+        public async Task<PatchAcceptCommandResponse> Handle(PatchAcceptCommand request, CancellationToken cancellationToken)
+        {
+            var id = request.Project(x => x.AcceptID);
+
+            var data = await DefaultDbContext.Accepts
+                .SingleOrDefaultAsync(x => x.AcceptID == id);
+
+            if (data == null)
+            {
+                throw new Exception("Accept not found!");
+            }
+
+            request.Patch(data);
+
+            await DefaultDbContext.SaveChangesAsync();
+
+            return new PatchAcceptCommandResponse(request, data, resultCount: 1);
+        }
+    }
+}
