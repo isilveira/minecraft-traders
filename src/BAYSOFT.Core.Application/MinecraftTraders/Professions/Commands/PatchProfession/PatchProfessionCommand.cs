62d9f486913b50cfa46c86d6b5c9c9639a26bbdf
Ítalo Silveira <italobrian@gmail.com>
2020-07-01 22:38:44 -0300

    Patch profession flow

---
+++ src/BAYSOFT.Core.Application/MinecraftTraders/Professions/Commands/PatchProfession/PatchProfessionCommand.cs
@@ -0,0 +1,21 @@
+﻿using BAYSOFT.Core.Domain.Entities.MinecraftTraders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BAYSOFT.Core.Application.MinecraftTraders.Professions.Commands.PatchProfession
+{
+    public class PatchProfessionCommand : ApplicationRequest<Profession, PatchProfessionCommandResponse>
+    {
+        public PatchProfessionCommand()
+        {
+            ConfigKeys(x => x.ProfessionID);
+
+            ConfigSuppressedProperties(x => x.Villagers);
+            ConfigSuppressedProperties(x => x.ProfessionItems);
+
+            ConfigSuppressedResponseProperties(x => x.Villagers);
+            ConfigSuppressedResponseProperties(x => x.ProfessionItems);
+        }
+    }
+}
---
+++ src/BAYSOFT.Core.Application/MinecraftTraders/Professions/Commands/PatchProfession/PatchProfessionCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using BAYSOFT.Core.Domain.Interfaces.Infrastructures.Contexts;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ModelWrapper.Extensions.Patch;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BAYSOFT.Core.Application.MinecraftTraders.Professions.Commands.PatchProfession
+{
+    public class PatchProfessionCommandHandler : IRequestHandler<PatchProfessionCommand, PatchProfessionCommandResponse>
+    {
+        private IDefaultDbContext DefaultDbContext { get; set; }
+        public PatchProfessionCommandHandler(IDefaultDbContext defaultDbContext)
+        {
+            DefaultDbContext = defaultDbContext;
+        }
+
+        public async Task<PatchProfessionCommandResponse> Handle(PatchProfessionCommand request, CancellationToken cancellationToken)
+        {
+            var id = request.Project(x => x.ProfessionID);
+
+            var data = await DefaultDbContext.Professions
+                .SingleOrDefaultAsync(x => x.ProfessionID == id, cancellationToken);
+
+            if (data == null)
+            {
+                throw new Exception("Profession not found!");
+            }
+
+            request.Patch(data);
+
+            await DefaultDbContext.SaveChangesAsync(cancellationToken);
+
+            return new PatchProfessionCommandResponse(request, data, resultCount: 1);
+        }
+    }
+}
