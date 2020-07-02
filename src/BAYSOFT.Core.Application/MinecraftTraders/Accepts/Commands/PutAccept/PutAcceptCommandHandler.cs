5e438d154aa81420edbf279261ee001fa01a99da
Ítalo Silveira <italobrian@gmail.com>
2020-07-01 23:12:20 -0300

    Put accept flow

---
+++ src/BAYSOFT.Core.Application/MinecraftTraders/Accepts/Commands/PutAccept/PutAcceptCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BAYSOFT.Core.Application.MinecraftTraders.Accepts.Commands.PutAccept
+{
+    public class PutAcceptCommandHandler : IRequestHandler<PutAcceptCommand, PutAcceptCommandResponse>
+    {
+        public PutAcceptCommandHandler()
+        {
+        }
+
+        public Task<PutAcceptCommandResponse> Handle(PutAcceptCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
