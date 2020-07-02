101c5c1e2f1f4f756c6e0f72f68b06504764d4ad
Ítalo Silveira <italobrian@gmail.com>
2020-07-01 23:34:47 -0300

    Delete offer flow

---
+++ src/BAYSOFT.Core.Application/MinecraftTraders/Offers/Commands/DeleteOffer/DeleteOfferCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BAYSOFT.Core.Application.MinecraftTraders.Offers.Commands.DeleteOffer
+{
+    public class DeleteOfferCommand
+    {
+    }
+}
---
+++ src/BAYSOFT.Core.Application/MinecraftTraders/Offers/Commands/DeleteOffer/DeleteOfferCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using BAYSOFT.Core.Domain.Interfaces.Infrastructures.Contexts;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BAYSOFT.Core.Application.MinecraftTraders.Offers.Commands.DeleteOffer
+{
+    public class DeleteOfferCommandHandler : IRequestHandler<DeleteOfferCommand, DeleteOfferCommandResponse>
+    {
+        public IDefaultDbContext DefaultDbContext { get; set; }
+        public DeleteOfferCommandHandler(IDefaultDbContext defaultDbContext)
+        {
+            DefaultDbContext = defaultDbContext;
+        }
+
+        public async Task<DeleteOfferCommandResponse> Handle(DeleteOfferCommand request, CancellationToken cancellationToken)
+        {
+            var id = request.Project(x => x.OfferID);
+
+            var data = await DefaultDbContext.Offers
+                .SingleOrDefaultAsync(x => x.OfferID == id);
+
+            if (data == null)
+            {
+                throw new Exception("Offer not found!");
+            }
+
+            DefaultDbContext.Offers.Remove(data);
+
+            await DefaultDbContext.SaveChangesAsync(cancellationToken);
+
+            return new DeleteOfferCommandResponse(request, data, resultCount: 1);
+        }
+    }
+}
