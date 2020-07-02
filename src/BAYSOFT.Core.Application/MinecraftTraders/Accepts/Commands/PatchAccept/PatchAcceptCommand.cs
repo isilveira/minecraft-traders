6b259a2533411e0cdedc679991e96cd6972f143e
Ítalo Silveira <italobrian@gmail.com>
2020-07-01 23:57:38 -0300

    Update patch accept flow

--- src/BAYSOFT.Core.Application/MinecraftTraders/Accepts/Commands/PatchAccept/PatchAcceptCommand.cs
+++ src/BAYSOFT.Core.Application/MinecraftTraders/Accepts/Commands/PatchAccept/PatchAcceptCommand.cs
@@ -5,10 +5,17 @@ using System.Text;
 
 namespace BAYSOFT.Core.Application.MinecraftTraders.Accepts.Commands.PatchAccept
 {
-    public class PatchAcceptCommand : ApplicationRequest<Villager, PatchAcceptCommandResponse>
+    public class PatchAcceptCommand : ApplicationRequest<Accept, PatchAcceptCommandResponse>
     {
         public PatchAcceptCommand()
         {
+            ConfigKeys(x => x.AcceptID);
+
+            ConfigSuppressedProperties(x => x.Item);
+            ConfigSuppressedProperties(x => x.Trade);
+
+            ConfigSuppressedResponseProperties(x => x.Item);
+            ConfigSuppressedResponseProperties(x => x.Trade);
         }
     }
 }
--- src/BAYSOFT.Core.Application/MinecraftTraders/Accepts/Commands/PatchAccept/PatchAcceptCommandResponse.cs
+++ src/BAYSOFT.Core.Application/MinecraftTraders/Accepts/Commands/PatchAccept/PatchAcceptCommandResponse.cs
@@ -6,13 +6,13 @@ using System.Text;
 
 namespace BAYSOFT.Core.Application.MinecraftTraders.Accepts.Commands.PatchAccept
 {
-    public class PatchAcceptCommandResponse : ApplicationResponse<Villager>
+    public class PatchAcceptCommandResponse : ApplicationResponse<Accept>
     {
         public PatchAcceptCommandResponse()
         {
         }
 
-        public PatchAcceptCommandResponse(WrapRequest<Villager> request, object data, string message = "Successful operation!", long? resultCount = null) : base(request, data, message, resultCount)
+        public PatchAcceptCommandResponse(WrapRequest<Accept> request, object data, string message = "Successful operation!", long? resultCount = null) : base(request, data, message, resultCount)
         {
         }
     }
