2c9a33d907b9fab7910d139cf86ce39e51202c6f
Ítalo Silveira <italobrian@gmail.com>
2020-07-01 23:35:26 -0300

    Put offer flow

---
+++ src/BAYSOFT.Core.Application/MinecraftTraders/Offers/Commands/PutOffer/PutOfferCommand.cs
@@ -0,0 +1,14 @@
+﻿using BAYSOFT.Core.Domain.Entities.MinecraftTraders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BAYSOFT.Core.Application.MinecraftTraders.Offers.Commands.PutOffer
+{
+    public class PutOfferCommand : ApplicationRequest<Offer, PutOfferCommandResponse>
+    {
+        public PutOfferCommand()
+        {
+        }
+    }
+}
---
+++ src/BAYSOFT.Core.Application/MinecraftTraders/Offers/Commands/PutOffer/PutOfferCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BAYSOFT.Core.Application.MinecraftTraders.Offers.Commands.PutOffer
+{
+    public class PutOfferCommandHandler : IRequestHandler<PutOfferCommand, PutOfferCommandResponse>
+    {
+        public PutOfferCommandHandler()
+        {
+        }
+
+        public Task<PutOfferCommandResponse> Handle(PutOfferCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
