1a66ac75a3854dd26c474221bdc5f32b3a285c55
Ítalo Silveira <italobrian@gmail.com>
2020-07-01 23:11:38 -0300

    Delete accept flow

---
+++ src/BAYSOFT.Core.Application/MinecraftTraders/Accepts/Commands/DeleteAccept/DeleteAcceptCommand.cs
@@ -0,0 +1,14 @@
+﻿using BAYSOFT.Core.Domain.Entities.MinecraftTraders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BAYSOFT.Core.Application.MinecraftTraders.Accepts.Commands.DeleteAccept
+{
+    public class DeleteAcceptCommand : ApplicationRequest<Villager, DeleteAcceptCommandResponse>
+    {
+        public DeleteAcceptCommand()
+        {
+        }
+    }
+}
---
+++ src/BAYSOFT.Core.Application/MinecraftTraders/Accepts/Commands/DeleteAccept/DeleteAcceptCommandHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BAYSOFT.Core.Application.MinecraftTraders.Accepts.Commands.DeleteAccept
+{
+    public class DeleteAcceptCommandHandler
+    {
+    }
+}
