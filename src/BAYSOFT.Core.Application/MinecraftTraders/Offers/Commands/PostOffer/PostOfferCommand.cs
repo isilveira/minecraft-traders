d0e64b9f520c66de09e3cfe498a74010ad7ec1c9
Ítalo Silveira <italobrian@gmail.com>
2020-07-01 23:35:15 -0300

    Post offer flow

---
+++ src/BAYSOFT.Core.Application/MinecraftTraders/Offers/Commands/PostOffer/PostOfferCommand.cs
@@ -0,0 +1,14 @@
+﻿using BAYSOFT.Core.Domain.Entities.MinecraftTraders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BAYSOFT.Core.Application.MinecraftTraders.Offers.Commands.PostOffer
+{
+    public class PostOfferCommand : ApplicationRequest<Offer, PostOfferCommandResponse>
+    {
+        public PostOfferCommand()
+        {
+        }
+    }
+}
---
+++ src/BAYSOFT.Core.Application/MinecraftTraders/Offers/Commands/PostOffer/PostOfferCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using BAYSOFT.Core.Domain.Interfaces.Infrastructures.Contexts;
+using MediatR;
+using ModelWrapper.Extensions.Post;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BAYSOFT.Core.Application.MinecraftTraders.Offers.Commands.PostOffer
+{
+    public class PostOfferCommandHandler : IRequestHandler<PostOfferCommand, PostOfferCommandResponse>
+    {
+        public IDefaultDbContext DefaultDbContext { get; set; }
+        public PostOfferCommandHandler(IDefaultDbContext defaultDbContext)
+        {
+            DefaultDbContext = defaultDbContext;
+        }
+
+        public async Task<PostOfferCommandResponse> Handle(PostOfferCommand request, CancellationToken cancellationToken)
+        {
+            var data = request.Post();
+
+            await DefaultDbContext.Offers.AddAsync(data, cancellationToken);
+
+            await DefaultDbContext.SaveChangesAsync(cancellationToken);
+
+            return new PostOfferCommandResponse(request, data, resultCount: 1);
+        }
+    }
+}
