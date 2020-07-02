8bf7d4bc1fb1aaf023604c0f997683a5d237488d
Ítalo Silveira <italobrian@gmail.com>
2020-07-01 23:12:07 -0300

    Post accept flow

---
+++ src/BAYSOFT.Core.Application/MinecraftTraders/Accepts/Commands/PostAccept/PostAcceptCommand.cs
@@ -0,0 +1,14 @@
+﻿using BAYSOFT.Core.Domain.Entities.MinecraftTraders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BAYSOFT.Core.Application.MinecraftTraders.Accepts.Commands.PostAccept
+{
+    public class PostAcceptCommand : ApplicationRequest<Villager, PostAcceptCommandResponse>
+    {
+        public PostAcceptCommand()
+        {
+        }
+    }
+}
---
+++ src/BAYSOFT.Core.Application/MinecraftTraders/Accepts/Commands/PostAccept/PostAcceptCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using BAYSOFT.Core.Domain.Interfaces.Infrastructures.Contexts;
+using MediatR;
+using ModelWrapper.Extensions.Post;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BAYSOFT.Core.Application.MinecraftTraders.Accepts.Commands.PostAccept
+{
+    public class PostAcceptCommandHandler : IRequestHandler<PostAcceptCommand, PostAcceptCommandResponse>
+    {
+        private IDefaultDbContext DefaultDbContext { get; set; }
+        public PostAcceptCommandHandler(IDefaultDbContext defaultDbContext)
+        {
+            DefaultDbContext = defaultDbContext;
+        }
+
+        public async Task<PostAcceptCommandResponse> Handle(PostAcceptCommand request, CancellationToken cancellationToken)
+        {
+            var data = request.Post();
+
+            await DefaultDbContext.Accepts.AddAsync(data, cancellationToken);
+
+            await DefaultDbContext.SaveChangesAsync(cancellationToken);
+
+            return new PostAcceptCommandResponse(request, data, resultCount: 1);
+        }
+    }
+}
